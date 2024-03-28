0e5880b8fdd3d05b234f4e706d29bd32f44a2998
CYPHIER091ASK <165249583+CYPHIER091ASK@users.noreply.github.com>
2024-03-28 12:21:08 +0530

    Update and rename DICTIONARY IN PY.py to operations in python.py

---
+++ operations in python.py
@@ -0,0 +1,34 @@
+num1 = 15
+num2 = 12
+
+sum = num1 + num2
+print("Sum of " , num1, " and ", num2 ," is ", sum)
+
+
+number1 = input("First number: ")
+number2 = input("\nSecond number: ")
+
+sum = float (number1) + float(number2)
+
+print("The sum of {0} and {1} is {2}" .format(number1,number2, sum))
+
+
+def add(a,b)
+  return a+b
+
+num1 = 203
+num2 = 23
+sum_of_the_two_numbers = add(num1,num2)
+print("Sum of the two numbers {0} and {1} is {2} " .format(num1,num2,sum_of_the_two_numbers))
+
+num1 = 14
+num2 = 23
+
+import operator
+su = operator.add(num1,num2)
+print("Sum of {}
+
+
+
+
+
